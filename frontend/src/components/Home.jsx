43ca24314933b028e10193ed00d8ca1b06247118
Arslan Javaid <aja118379@gmail.com>
2024-09-14 20:49:58 +0500

    first commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+import Navbar from './shared/Navbar'
+import Footer from './shared/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+import { LogOut, User2 } from 'lucide-react'
+
+
+const Navbar = () => {
+  return (
+    <>
+        <h1>Hello Arslan inside Navbar</h1>
+        <LogOut />
+        <User2 />
+    </>
+  )
+}
+
+export default Navbar
