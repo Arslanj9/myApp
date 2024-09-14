d93e80383d7d2ee38342c86eb0bd9beb59a7aaff
Arslan Javaid <aja118379@gmail.com>
2024-09-14 21:20:26 +0500

    Feature 2 1st Commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
       <Navbar />
       <Footer />
       <h1>This is a new text inside feature1 branch</h1>
+      <h2>This is a new text inside feature2 branch</h2>
     </>
   )
 }
